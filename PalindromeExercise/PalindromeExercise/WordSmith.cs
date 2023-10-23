004305f9c3a10e0068b5ebe5b6d510c550b0b434
sarajadidi <sararivkajadidi@gmail.com>
2023-10-23 10:16:49 -0700

    finished second exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		
+        public bool IsAPalendrome(string word)
+        {
+            //reverse the word see that is it the same after reversed
+            var reversed = "";
+            for(int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+            if(reversed == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            //compare with original
+            //retrun whether true or false/ same or not
+        }
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,30 @@
 using System;
+using PalindromeExercise;
 using Xunit;
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hey", false)]
+        [InlineData("civic", true)]
+        [InlineData("noon", true)]
+        [InlineData("howdy", false)]
+        [InlineData("hannah", true)]
+        [InlineData("brother", false)]
+        public void Test1(string name, bool expected)
         {
+            //arrange
+            var pal = new WordSmith();
 
+            //act
+            var actual = pal.IsAPalendrome(name);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
